cfbb06cd2d71eb52919372069af133ea00b8ccfd
joe <joe.graphica@gmail.com>
2026-03-16 17:31:06 +0900

    Strengthen TikTok in-app browser detection

--- TikRIng/src/utils/browser.ts
+++ TikRIng/src/utils/browser.ts
@@ -1,4 +1,30 @@
 export function isTikTokInAppBrowser(): boolean {
   const ua = navigator.userAgent || '';
-  return /TikTok|musical_ly|BytedanceWebview|aweme/i.test(ua);
+  const uaLower = ua.toLowerCase();
+
+  const tiktokUaSignatures = [
+    'tiktok',
+    'musical_ly',
+    'bytedancewebview',
+    'ttwebview',
+    'aweme',
+    'trill',
+    'tiktoklite',
+    'com.zhiliaoapp.musically',
+  ];
+
+  const isTikTokUa = tiktokUaSignatures.some((signature) => uaLower.includes(signature));
+  if (isTikTokUa) {
+    return true;
+  }
+
+  const uaData = navigator.userAgentData as { brands?: Array<{ brand: string }> } | undefined;
+  const brands = uaData?.brands ?? [];
+  const hasTikTokBrand = brands.some((entry) => /tiktok|bytedance|ttwebview/i.test(entry.brand));
+  if (hasTikTokBrand) {
+    return true;
+  }
+
+  const referrer = document.referrer || '';
+  return /tiktok\.com|tiktokv\.com|musical\.ly/i.test(referrer);
 }
